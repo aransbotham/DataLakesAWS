31a121c8beb95bdfc26587e96a221960212b2354
Anna Ransbotham-Cole <annaransbotham-cole@annas-mbp.lan>
2024-03-03 10:25:48 -0600

    use corrected serialNumber

--- script/customer_trusted_to_curated.py
+++ script/customer_trusted_to_curated.py
@@ -21,13 +21,6 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node Customer Trusted
-CustomerTrusted_node1709480695760 = glueContext.create_dynamic_frame.from_catalog(
-    database="stedi",
-    table_name="customer_trusted",
-    transformation_ctx="CustomerTrusted_node1709480695760",
-)
-
 # Script generated for node Accelerometer Trusted
 AccelerometerTrusted_node1709480712591 = glueContext.create_dynamic_frame.from_catalog(
     database="stedi",
@@ -35,20 +28,54 @@ AccelerometerTrusted_node1709480712591 = glueContext.create_dynamic_frame.from_c
     transformation_ctx="AccelerometerTrusted_node1709480712591",
 )
 
+# Script generated for node Customer Trusted
+CustomerTrusted_node1709480695760 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="customer_trusted",
+    transformation_ctx="CustomerTrusted_node1709480695760",
+)
+
+# Script generated for node Step Trainer Landing
+StepTrainerLanding_node1709482603255 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="step_trainer_landing",
+    transformation_ctx="StepTrainerLanding_node1709482603255",
+)
+
+# Script generated for node Fetch Correct Serial Numbers
+SqlQuery1 = """
+select 
+distinct 
+at.user as email,
+stl.serialNumber
+from at 
+join stl ON at.timestamp = stl.sensorReadingTime
+"""
+FetchCorrectSerialNumbers_node1709482644276 = sparkSqlQuery(
+    glueContext,
+    query=SqlQuery1,
+    mapping={
+        "at": AccelerometerTrusted_node1709480712591,
+        "stl": StepTrainerLanding_node1709482603255,
+    },
+    transformation_ctx="FetchCorrectSerialNumbers_node1709482644276",
+)
+
 # Script generated for node Keep Customers with Accelerometer Data
 SqlQuery0 = """
 select distinct
-ct.*
+ct.*,
+serial.serialNumber as corrected_serialNumber
 from ct 
-JOIN at 
-ON ct.email = at.user
+JOIN serial 
+ON ct.email = serial.email
 """
 KeepCustomerswithAccelerometerData_node1709424633360 = sparkSqlQuery(
     glueContext,
     query=SqlQuery0,
     mapping={
         "ct": CustomerTrusted_node1709480695760,
-        "at": AccelerometerTrusted_node1709480712591,
+        "serial": FetchCorrectSerialNumbers_node1709482644276,
     },
     transformation_ctx="KeepCustomerswithAccelerometerData_node1709424633360",
 )
